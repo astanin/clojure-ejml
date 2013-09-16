3513e5cd0c673ca4ed1ad78d85b4cf1e527727ce
Sergey Astanin <s.astanin@gmail.com>
2013-09-16 16:43:57 +0200

    PHasNorm protocol

--- src/clojure/core/linalg.clj
+++ src/clojure/core/linalg.clj
@@ -50,3 +50,27 @@ PMatrixOps can be extended with (cond). Norms deserve to be a separate
 protocol as also non-matrix types may implement them (PVectorDistance
 may be extended to support non-Euclidean distances too)."
 )
+
+
+(defprotocol PHasNorm
+  "Protocol for types which have at least one norm.
+
+  All implementations should implement the default norm as the
+  unary (norm v) method. Imlementations with more than one norm should
+  implement binary (norm v norm-id) method.
+
+  The following convention should be used for matrices and
+  vectors (similar to NumPy):
+
+  ========  =============================  ==========================
+  norm-id   norm for matrices              norm for vectors
+  ========  =============================  ==========================
+  0         nil                            number of non-zero values
+  1         max column-sum of abs. values  sum of absolute values
+  2         2-norm (largest sing. value)   Euclidean norm (default)
+  :inf      max row-sum of abs. values     max of absolute values
+  :fro      Frobenius norm (default)       nil
+
+"
+  (norm [m] "Calculates default norm.")
+  (norm [m norm-id] "Calculates a norm identified with norm-id."))
