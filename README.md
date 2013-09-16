4fc81b0e94736c54f8ccd60f19032358faea6f18
Sergey Astanin <s.astanin@gmail.com>
2013-09-16 12:34:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# clojure-ejml
+
+Efficient Java Matrix Library now for Clojure.
+
+Implementation of [core.matrix](https://github.com/mikera/matrix-api#readme)
+and linear algebra APIs for EJML data types.
+
+## Usage
+
+[API documentation](http://astanin.github.io/clojure-ejml/)
+
+## License
+
+Copyright © 2013 Sergey Astanin
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojure-ejml "0.1.0-SNAPSHOT"
+  :description "Efficient Java Matrix Library now for Clojure"
+  :url "http://github.com/astanin/clojure-ejml"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.googlecode.efficient-java-matrix-library/ejml "0.23"]
+                 [net.mikera/core.matrix "0.10.0"]])
