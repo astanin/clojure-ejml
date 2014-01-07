96a3b39d21a4034eb28cc183e31e6f7e17f42b9b
Sergey Astanin <s.astanin@gmail.com>
2014-01-07 13:12:06 +0100

    update dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.googlecode.efficient-java-matrix-library/ejml "0.23"]
-                 [net.mikera/core.matrix "0.10.0"]]
-  :profiles {:dev {:plugins [[codox "0.6.4"]  ; lein with-profile dev doc
+                 [com.googlecode.efficient-java-matrix-library/ejml "0.24"]  ;; Apache 2.0
+                 [net.mikera/core.matrix "0.18.0"]  ;; EPL 1.0
+                 ]
+  :profiles {:dev {:plugins [[codox "0.6.6"]  ; lein with-profile dev doc
                              ]}})
